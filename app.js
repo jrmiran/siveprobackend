5650e67791dff51823ec24db04398aca8d02b02a
jrmiran <46909195+jrmiran@users.noreply.github.com>
2019-05-14 14:36:32 -0300

    make it better

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ require('./app/routes/query')(app, "/clientsFisico",
                               "SELECT nome FROM Cliente where Cliente.DTYPE = 'Pessoa'");
 require('./app/routes/query')(app, "/clientsArquiteto",
                               "SELECT nome FROM Cliente where Cliente.DTYPE = 'Arquiteto'");
-require('./app/routes/query')(app, "/clientsEmpresa/nameEmpresa",
+require('./app/routes/query')(app, "/clientsEmpresa:nameEmpresa",
                               "Select ClienteEmpresa.nome from ClienteEmpresa, Cliente where ClienteEmpresa.empresa_id = Cliente.id and Cliente.nome = nameEmpresa");
 
 app.listen(porta);
