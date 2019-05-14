f1f283d471e00a1942890afb27dc9c7e5f1c1114
jrmiran <46909195+jrmiran@users.noreply.github.com>
2019-05-14 16:13:10 -0300

    make it better

--- app/routes/queryParam.js
+++ app/routes/queryParam.js
@@ -6,7 +6,7 @@ module.exports = function(app){
     
     app.get("/clientsEmpresa2/:nameEmpresa", function(req, res){  
         console.log(req.params.nameEmpresa)
-        let sql = `Select ClienteEmpresa.nome from ClienteEmpresa, Cliente where ClienteEmpresa.empresa_id = Cliente.id and Cliente.nome = ${req.params.nameEmpresa}`;
+        let sql = "Select ClienteEmpresa.nome from ClienteEmpresa, Cliente where ClienteEmpresa.empresa_id = Cliente.id and Cliente.nome = 'Alameda'";
         
         con.query(sql, function(err, result){
             res.send(result);
