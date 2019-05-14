6638697c87a50dcbc693eee1a76a051d4fb7f9ea
jrmiran <46909195+jrmiran@users.noreply.github.com>
2019-05-14 15:45:12 -0300

    make it better

--- app/routes/queryParam.js
+++ app/routes/queryParam.js
@@ -5,6 +5,7 @@ module.exports = function(app){
     var con = dbConnection();
     
     app.get("/clientsEmpresa2/:nameEmpresa", function(req, res){  
+        console.log(req.params.nameEmpresa)
         con.query("Select ClienteEmpresa.nome from ClienteEmpresa, Cliente where ClienteEmpresa.empresa_id = Cliente.id and Cliente.nome = " + req.params.nameEmpresa, function(err, result){
             res.send(result);
         });
