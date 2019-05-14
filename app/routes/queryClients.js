99fdc74769d2867c84e989c7e619e2927edc572a
jrmiran <46909195+jrmiran@users.noreply.github.com>
2019-05-14 16:34:08 -0300

    make it better

--- app/routes/queryClients.js
+++ app/routes/queryClients.js
@@ -4,7 +4,7 @@ var dbConnection = require('../../config/dbConnection');
 module.exports = function(app, client){
     var con = dbConnection();
     
-    app.get("/"+client+"/:nameEmpresa", function(req, res){  
+    app.get(`/${client}/:nameEmpresa`, function(req, res){  
         console.log(req.params.nameEmpresa)
         let sql = `Select ClienteEmpresa.nome from ClienteEmpresa, Cliente where ClienteEmpresa.empresa_id = Cliente.id and Cliente.nome = ${req.params.nameEmpresa}`;
         
