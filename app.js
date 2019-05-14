8b48573e52ace68d572298f96036709cb4b44080
jrmiran <46909195+jrmiran@users.noreply.github.com>
2019-05-14 16:31:46 -0300

    make it better

--- app.js
+++ app.js
@@ -14,9 +14,7 @@ require('./app/routes/query')(app, "/clientsFisico",
                               "SELECT nome FROM Cliente where Cliente.DTYPE = 'Pessoa'");
 require('./app/routes/query')(app, "/clientsArquiteto",
                               "SELECT nome FROM Cliente where Cliente.DTYPE = 'Arquiteto'");
-require('./app/routes/query')(app, "/clientsEmpresa/:nameEmpresa",
-                              `Select ClienteEmpresa.nome from ClienteEmpresa, Cliente where ClienteEmpresa.empresa_id = Cliente.id and Cliente.nome = ${req.params.nameEmpresa}`);
-require('./app/routes/queryParam')(app);
+require('./app/routes/queryParam')(app,"clientEmpresa");
 
 app.listen(porta);
 /*app.listen(3000, function(){
--- app/routes/queryClients.js
+++ app/routes/queryClients.js
@@ -1,10 +1,10 @@
 var dbConnection = require('../../config/dbConnection');
 
 
-module.exports = function(app){
+module.exports = function(app, client){
     var con = dbConnection();
     
-    app.get("/clientsEmpresa2/:nameEmpresa", function(req, res){  
+    app.get("/"+client+"/:nameEmpresa", function(req, res){  
         console.log(req.params.nameEmpresa)
         let sql = `Select ClienteEmpresa.nome from ClienteEmpresa, Cliente where ClienteEmpresa.empresa_id = Cliente.id and Cliente.nome = ${req.params.nameEmpresa}`;
         
